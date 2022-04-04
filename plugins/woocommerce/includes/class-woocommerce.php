24261aeed2b90a7c55c21d598d8ea7917fba564d
roykho <roykho77@gmail.com>
2022-04-04 12:07:37 -0700

    Remove hook

--- plugins/woocommerce/includes/class-woocommerce.php
+++ plugins/woocommerce/includes/class-woocommerce.php
@@ -374,12 +374,7 @@ final class WooCommerce {
 		 * Class autoloader.
 		 */
 		include_once WC_ABSPATH . 'includes/class-wc-autoloader.php';
-/**
- * Test hook
- *
- * @since 6.4.0
- */
-do_action('woocommerce_test_hook');
+
 		/**
 		 * Interfaces.
 		 */
